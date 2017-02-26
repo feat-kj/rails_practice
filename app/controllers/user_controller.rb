40698d5883228e6f64b83c0003bae985a2f1f9e1
kohhata <kohhata@opendoor.co.jp>
2017-02-26 19:02:05 +0900

    エラーメッセージ調整

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -18,38 +18,36 @@ class UserController < ApplicationController
 
   def create
     @user = User.new(user_param)
+    @prefectures = Prefecture.order(:sort)
+    @categories  = Category.order(:sort)
+    if params[:genre_ids].blank?
+      params[:genre_ids] = []
+    end
 
     @user_genres = []
     for genre_id in params[:genre_ids]
-      user_genre = UserGenre.new
-      user_genre.genre_id = genre_id.to_i
       @user_genres.push(user_genre)
     end
-
     print @user_genres.count
 
-    if !@user.valid?
-      @prefectures = Prefecture.order(:sort)
-      @categories = Category.order(:sort)
+    if @user.valid?
+      User.transaction do
+        @user.save!
+        @user_genres = []
+        for genre_id in params[:genre_ids]
+          user_genre = UserGenre.new
+          user_genre.genre_id = genre_id.to_i
+          user_genre.user_id = @user.id
+          user_genre.save!
+          @user_genres.push(user_genre)
+        end
+      end
+      redirect_to :action => "show", :id => @user.id
+
+    else
       render :action => "new"
     end
 
-    User.transaction do
-      @user.save!
-      @user_genres = []
-      for genre_id in params[:genre_ids]
-        user_genre = UserGenre.new
-        user_genre.genre_id = genre_id.to_i
-        user_genre.user_id = @user.id
-        user_genre.save!
-        @user_genres.push(user_genre)
-      end
-    end
-
-    redirect_to :action => "show", :id => @user.id
-
-    rescue => e
-      render :text => "エラー"
   end
 
 
